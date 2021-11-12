5c14ff300b1c8d7ff35066c55aa2aa1ea293c0b9
AshwiniChavhan9 <ashwini.chavhan910@gmail.com>
2021-11-12 12:23:01 +0530

    url mappings

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from first_app import views
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index , name='index')
+    path('',views.index , name='index'),
+    path('django_project/', include('first_app.urls'))
 ]
